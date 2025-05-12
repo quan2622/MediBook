7f46ac7fbf1c71dbe4bc58b19439eb6544dec9f0
Quan Nguyen Hong <155934975+quan2622@users.noreply.github.com>
2025-05-12 09:53:47 +0700

    update model user, history, clinic, specialty & migration

--- src/migrations/migration-create-user.js
+++ src/migrations/migration-create-user.js
@@ -26,10 +26,16 @@ module.exports = {
       gender: {
         type: Sequelize.BOOLEAN
       },
-      typeRole: {
+      roleId: {
         type: Sequelize.STRING
       },
-      keyRole: {
+      positionId: {
+        type: Sequelize.STRING
+      },
+      phoneNumber: {
+        type: Sequelize.STRING
+      },
+      image: {
         type: Sequelize.STRING
       },
       createdAt: {
