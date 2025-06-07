9286ef878edd1ab61a6b102540c9dfee1cf7a5e5
Quan Nguyen Hong <155934975+quan2622@users.noreply.github.com>
2025-06-07 15:54:15 +0700

    render schedule data

--- frontend/src/containers/Patient/Doctor/DoctorSchedule.js
+++ frontend/src/containers/Patient/Doctor/DoctorSchedule.js
@@ -7,6 +7,7 @@ import { LANGUAGES } from "../../../utils";
 import Select from 'react-select'
 import moment from 'moment'
 import localization from 'moment/locale/vi'
+import _ from "lodash";
 
 const select_style = {
   container: (provided) => ({
@@ -41,19 +42,38 @@ class DoctorSchedule extends Component {
 
   buildSelectOptions = () => {
     const { language } = this.props;
+    const { selectedOption, allDays } = this.state;
     let arrDay = [];
     for (let i = 0; i < 7; i++) {
+      const date = moment(new Date()).add(i, 'days');
+      let label = '';
+
+      if (language === LANGUAGES.VI) {
+        const raw = date.format('dddd - DD/MM');
+        if (i === 0) {
+          const index = raw.indexOf('-');
+          label = ['Hôm nay', raw.substring(index + 1)].join(' - ');
+        } else label = raw[0].toUpperCase() + raw.slice(1);
+      } else {
+        const raw = date.locale('en').format('ddd - DD/MM');
+        if (i === 0) {
+          const index = raw.indexOf('-');
+          label = ['Today', raw.substring(index + 1)].join(' - ');
+        } else label = raw;
+      }
+
       arrDay.push(({
-        label:
-          language === LANGUAGES.VI ?
-            moment(new Date()).add(i, 'days').format('dddd - DD/MM')
-            :
-            moment(new Date()).add(i, 'days').locale('en').format('ddd - DD/MM'),
+        label: label,
         value: moment(new Date()).add(i, 'days').startOf('day').valueOf(),
       }));
     }
-    this.setState({ allDays: arrDay, selectedOption: arrDay[0] });
-    this.handleOnchangeSelect(arrDay[0]);
+
+    let indexSelect = 0;
+    if (allDays.length > 0 && selectedOption !== null) {
+      indexSelect = allDays.findIndex(item => _.isEqual(item, selectedOption));
+    }
+    this.setState({ allDays: arrDay, selectedOption: arrDay[indexSelect] });
+    this.handleOnchangeSelect(arrDay[indexSelect]);
   }
 
   handleOnchangeSelect = (select) => {
@@ -66,6 +86,7 @@ class DoctorSchedule extends Component {
 
   render() {
     const { allDays, selectedOption } = this.state;
+    const { scheduleDoctor, language } = this.props;
     console.log("Check data schedule doctor: ", this.props.scheduleDoctor)
     return (
       <div className="doctor-schedule-container">
@@ -74,11 +95,29 @@ class DoctorSchedule extends Component {
             <Select options={allDays} value={selectedOption}
               styles={select_style}
               isClearable={false} onChange={this.handleOnchangeSelect}
+              classNamePrefix="custom"
             />
           }
         </div>
         <div className="all-time-available">
-
+          <div className="text-calender">
+            <span><i className="far fa-calendar-alt"></i> Lịch khám  </span>
+          </div>
+          <div className="time-content">
+            {scheduleDoctor && scheduleDoctor.length > 0 ?
+              scheduleDoctor.map(item => {
+                const { scheduleData } = item
+                const date = language === LANGUAGES.VI ? scheduleData.valueVi : scheduleData.valueEn;
+                return (
+                  <span className="box-time" key={item.id}>{date}</span>
+                )
+              })
+              :
+              <div className="">
+                Không có lịch hẹn trong ngày, vui lòng chọn thời gian khác!
+              </div>
+            }
+          </div>
         </div>
       </div>
     );
